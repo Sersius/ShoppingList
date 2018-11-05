9be124fbad76dc50d87db93076184956869e625a
Sersius <segoro97@msn.com>
2018-11-05 20:06:18 +0100

    Todo 4, checking for emulator

--- app/src/main/java/info/pauek/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/info/pauek/shoppinglist/ShoppingListActivity.java
@@ -1,10 +1,16 @@
 package info.pauek.shoppinglist;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -35,12 +41,13 @@ public class ShoppingListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_shopping_list);
 
         items = new ArrayList<>();
-        items.add(new ShoppingItem("Potatoes"));
-        items.add(new ShoppingItem("Toilet Paper"));
+        items.add(new ShoppingItem("Potatoes", false));
+        items.add(new ShoppingItem("Toilet Paper",false));
 
         items_view = findViewById(R.id.items_view);
         btn_add = findViewById(R.id.btn_add);
         edit_box = findViewById(R.id.edit_box);
+        edit_box.setText("");
 
         adapter = new ShoppingListAdapter(this, items);
 
@@ -53,9 +60,78 @@ public class ShoppingListActivity extends AppCompatActivity {
         adapter.setOnClickListener(new ShoppingListAdapter.OnClickListener() {
             @Override
             public void onClick(int position) {
-                String msg = "Has clicat: " + items.get(position).getName();
+                ShoppingItem item = items.get(position);
+                item.CheckedToggle();
+                adapter.notifyItemChanged(position);
+                String msg = "You clicked: " + items.get(position).getName();
                 Toast.makeText(ShoppingListActivity.this, msg, Toast.LENGTH_SHORT).show();
             }
         });
+
+
+    adapter.setOnLongClickListener(new ShoppingListAdapter.OnLongClickListener() {
+        @Override
+        public void onLongClick(final int position) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(ShoppingListActivity.this);
+
+            builder.setTitle(R.string.confirmation)
+                    .setMessage(R.string.remove_confirmation)
+                    .setPositiveButton(R.string.remove, new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            items.remove(position);
+                            adapter.notifyItemRemoved(position);
+
+                            Toast.makeText(ShoppingListActivity.this, "Selected item has been removed", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .setNegativeButton(android.R.string.cancel, null);
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+    });
+
+}
+
+    public void onClickAddBtn(View view)
+    {
+        String name = edit_box.getText().toString();
+
+        if(name.isEmpty() == false)
+        {
+            items.add(new ShoppingItem(name,false));
+            adapter.notifyItemInserted(items.size()-1);
+            edit_box.setText("");
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_file,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int i = 0;
+
+        while(i < items.size())
+        {
+            if(items.get(i).isChecked())
+            {
+                items.remove(i);
+                adapter.notifyItemRemoved(i);
+            }
+            else
+                i++;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
