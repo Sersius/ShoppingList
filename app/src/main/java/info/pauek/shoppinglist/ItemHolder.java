73fbbb49adaca1f20275af18fffa7735742838ac
Sersius <segoro97@msn.com>
2018-11-05 20:28:30 +0100

    Added name_view forgot :(

--- app/src/main/java/info/pauek/shoppinglist/ItemHolder.java
+++ app/src/main/java/info/pauek/shoppinglist/ItemHolder.java
@@ -4,13 +4,13 @@ import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.CheckBox;
-import android.widget.TextView;
 
 public class ItemHolder extends RecyclerView.ViewHolder {
     private CheckBox name_view;
 
     public ItemHolder(@NonNull View itemView, final ShoppingListAdapter.OnClickListener onClickListener, final ShoppingListAdapter.OnLongClickListener onLongClickListener) {
         super(itemView);
+        name_view = itemView.findViewById(R.id.name_view);
         itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
