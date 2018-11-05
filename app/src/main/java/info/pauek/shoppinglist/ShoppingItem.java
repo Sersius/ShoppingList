887e32af73cdd3ae5387331015e3cb9272a2fb29
Sersius <segoro97@msn.com>
2018-11-05 19:22:38 +0100

    Todo 1 checkBox and Todo 2 all Layout

--- app/src/main/java/info/pauek/shoppinglist/ShoppingItem.java
+++ app/src/main/java/info/pauek/shoppinglist/ShoppingItem.java
@@ -2,16 +2,36 @@ package info.pauek.shoppinglist;
 
 public class ShoppingItem {
     private String name;
+    private boolean checkBox;
 
-    public ShoppingItem(String name) {
+    public ShoppingItem(String name, boolean checkBox)
+    {
         this.name = name;
+        this.checkBox = checkBox;
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name)
+    {
         this.name = name;
+    }
+
+    public boolean isChecked()
+    {
+        return checkBox;
+    }
+
+    public void setChecked(boolean checkBox)
+    {
+        this.checkBox = checkBox;
+    }
+
+    public void CheckedToggle()
+    {
+        this.checkBox = !checkBox;
     }
 }
